6fbbd3e20b6e7aa27f0cd2b5aba4ec5bed001f51
Teo Gebhard <teo@streamr.com>
2023-01-25 14:00:30 +0200

    iteration delay

--- bin/crawler.ts
+++ bin/crawler.ts
@@ -5,14 +5,17 @@ import 'reflect-metadata'
 import Container from 'typedi'
 import { CONFIG_TOKEN, readFromFile } from '../src/Config'
 import { Crawler } from '../src/crawler/Crawler'
+import { wait } from '@streamr/utils'
 
 const main = async () => {
     const configFile = process.argv[2]
-    Container.set(CONFIG_TOKEN, await readFromFile(configFile))
+    const config = await readFromFile(configFile)
+    Container.set(CONFIG_TOKEN, config)
     const crawler = Container.get(Crawler)
     // eslint-disable-next-line no-constant-condition
     while (true) {
         await crawler.updateStreams()
+        await wait(config.crawler.iterationDelay)
     }
 }
 
--- src/Config.ts
+++ src/Config.ts
@@ -12,6 +12,7 @@ export interface Config {
     }
     crawler: {
         subscribeDuration: number
+        iterationDelay: number
     }
     database: {
         host: string
