f77fc0545a393a3dbb62672e3f13559b7baebdff
Teo Gebhard <teo@streamr.com>
2025-01-31 14:08:32 +0200

    feat: [NET-1393] Collect RTTs (#30)
    The crawler collects RTT information from the nodes. 
    
    There is a new `Neighbor#rtt` field which stores the RTT value for a
    node pair:
    - if we have RTT value for both of the neighbor nodes, the RTT is the
    average of those value
    - otherwise it is the plain value from the node we received the value
    from, or `null` if no RTT value was available

--- src/crawler/Topology.ts
+++ src/crawler/Topology.ts
@@ -3,9 +3,14 @@ import { DhtAddress, StreamPartID } from '@streamr/sdk'
 import { Multimap, numberToIpv4, StreamPartIDUtils } from '@streamr/utils'
 import { NormalizedNodeInfo } from './NetworkNodeFacade'
 
+export interface Neighbor {
+    nodeId: DhtAddress
+    rtt?: number
+}
+
 export interface Node {
     id: DhtAddress
-    streamPartNeighbors: Multimap<StreamPartID, DhtAddress>
+    streamPartNeighbors: Multimap<StreamPartID, Neighbor>
     ipAddress?: string
 }
 
@@ -13,14 +18,14 @@ export class Topology {
 
     private nodes: Map<DhtAddress, Node> = new Map()
 
-    constructor(infos: NormalizedNodeInfo[]) {
+    constructor(infos: Pick<NormalizedNodeInfo, 'peerDescriptor' | 'streamPartitions'>[]) {
         const nodeIds = new Set(...[infos.map((info) => toNodeId(info.peerDescriptor))])
         for (const info of infos) {
-            const streamPartNeighbors: Multimap<StreamPartID, DhtAddress> = new Multimap()
+            const streamPartNeighbors: Multimap<StreamPartID, Neighbor> = new Multimap()
             for (const streamPartitionInfo of info.streamPartitions) {
                 const neighbors = streamPartitionInfo.contentDeliveryLayerNeighbors
-                    .map((n) => toNodeId(n.peerDescriptor))
-                    .filter((id) => nodeIds.has(id))
+                    .map((n) => ({ nodeId: toNodeId(n.peerDescriptor), rtt: n.rtt }))
+                    .filter((n) => nodeIds.has(n.nodeId))
                 streamPartNeighbors.addAll(StreamPartIDUtils.parse(streamPartitionInfo.id), neighbors)
             }
             const nodeId = toNodeId(info.peerDescriptor)
@@ -36,18 +41,24 @@ export class Topology {
         return [...this.nodes.values()]
     }
 
-    getNeighbors(nodeId: DhtAddress, streamPartId: StreamPartID): DhtAddress[] {
+    getNeighbors(nodeId: DhtAddress, streamPartId: StreamPartID): Neighbor[] {
         return this.nodes.get(nodeId)?.streamPartNeighbors.get(streamPartId) ?? []
     }
 
-    getPeers(streamPartId: StreamPartID): Set<DhtAddress> {
+    // gets the neighbor if nodeId2 is a neighbor of nodeId1 (but doesn't check the neighborhood other way around)
+    getNeighbor(nodeId1: DhtAddress, nodeId2: DhtAddress, streamPartId: StreamPartID): Neighbor | undefined {
+        const neighbors = this.nodes.get(nodeId1)?.streamPartNeighbors.get(streamPartId) ?? []
+        return neighbors.find((n) => n.nodeId === nodeId2)
+    }
+
+    getPeerNodeIds(streamPartId: StreamPartID): Set<DhtAddress> {
         const nodeIds: Set<DhtAddress> = new Set()
         for (const node of this.nodes.values()) {
             const neighbors = node.streamPartNeighbors.get(streamPartId)
             if (neighbors.length > 0) {
                 nodeIds.add(node.id)
                 for (const neighbor of neighbors) {
-                    nodeIds.add(neighbor)
+                    nodeIds.add(neighbor.nodeId)
                 }
             }
         }
